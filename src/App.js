1b6e00b7e3df6a8f1adc6e68334217de1aca614c
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-07-16 12:24:57 -0400

    added icon design

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component{
 			</Route> 
 
 		</Switch>
-		<footer class="bg-dark footer-section pt-2">
+		<footer class="bg-dark footer-section pt-2 fixed-bottom">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-4">
@@ -186,19 +186,36 @@ class About extends React.Component{
 		super(props);
 		this.placeholder_css = {
 			display: 'flex',
-			alignItems: 'center',
+			flexDirection: 'column',
 			justifyContent: 'center',
-			height: '83vh'
-			
+			alignItems: 'center',
+			height: '83vh',
 		};
 	}
 	render(){
 		return(
-			<div>
+			<div class="container" style={this.placeholder_css}> 
 				{/*  About */}
 				<div>
-					<div class="container" style={this.placeholder_css}>
+					<div class="container">
 					    Mastery is a solo-developed web application that has the intention to help students achieve their goals.
+					</div>
+					
+				</div>
+				<div class="container-fluid">
+					<div class="row" style={{justifyContent: 'center'}}>
+						<div class="col-3">
+							<img src={require("./icons8-loading-bar-60.png")}></img>
+							<h5>Tracker</h5>
+						</div>
+						<div class="col-3">	
+							<img src={require("./icons8-graph-64.png")}></img>
+							<h5>Stats</h5>
+						</div>
+						<div class="col-3">
+							<img src={require("./icons8-gift-64.png")}></img>
+							<h5>Rewards</h5>
+						</div>		
 					</div>
 				</div>
 			</div>
@@ -206,4 +223,4 @@ class About extends React.Component{
 	}
 }
 	
-export default App;
+export default App;	
