b85bbf933797aad76c116046b6cfb9b5107375e8
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-07-26 21:02:24 -0400

    fixed icon centering

--- src/App.js
+++ src/App.js
@@ -203,37 +203,36 @@ class About extends React.Component{
 			display: 'flex',
 			flexDirection: 'column',
 			justifyContent: 'center',
-			alignItems: 'center',
 			height: '83vh',
 		};
 	}
 	render(){
 		return(
-			<div class="container" style={this.placeholder_css}> 
-				{/*  About */}
-				<div>
-					<div class="container">
-					    Mastery is a solo-developed web application that has the intention to help students achieve their goals.
-					</div>
-					
-				</div>
-				<div class="container-fluid">
-					<div class="row" style={{justifyContent: 'center'}}>
-						<div class="col-3">
-							<img src={require("./icons8-loading-bar-60.png")}></img>
-							<h5>Tracker</h5>
-						</div>
-						<div class="col-3">	
-							<img src={require("./icons8-graph-64.png")}></img>
-							<h5>Stats</h5>
-						</div>
-						<div class="col-3">
-							<img src={require("./icons8-gift-64.png")}></img>
-							<h5>Rewards</h5>
-						</div>		
-					</div>
-				</div>
-			</div>
+                  <div style={this.placeholder_css}>
+                      <div class="d-flex justify-content-center"> 
+                              {/*  About */}
+                              <div>
+                                      <div>
+                                          Mastery is a solo-developed web application that has the intention to help students achieve their goals.
+                                      </div>
+                                      
+                              </div>
+                      </div>
+                      <div class="d-flex justify-content-around">
+                                      <div>
+                                              <img src={require("./icons8-loading-bar-60.png")}></img>
+                                              <h5>Tracker</h5>
+                                      </div>
+                                      <div>	
+                                              <img src={require("./icons8-graph-64.png")}></img>
+                                              <h5>Stats</h5>
+                                      </div>
+                                      <div>
+                                              <img src={require("./icons8-gift-64.png")}></img>
+                                              <h5>Rewards</h5>
+                                      </div>		
+                      </div>
+                  </div>
 		)
 	}
 }
