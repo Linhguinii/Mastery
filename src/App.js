45f99c5a2a305b28de90929fa0092501c5d9d607
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-07-29 18:45:52 -0400

    first login form

--- src/App.js
+++ src/App.js
@@ -240,11 +240,37 @@ class About extends React.Component{
 class LogIn extends React.Component{
 	constructor(props){
 		super(props);
+                this.body = {
+                  background: "#3598dc"
+                }
 	}
 	render(){
 		return(
 			<div>
-		
+                          <div class="signup-form">
+                            <form action="" method="post">
+                              <h2>Create your account</h2>
+                              <div class="form-group">
+                                <div class="row">
+                                  <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></input></div>
+                                  <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></input></div>
+                                </div>
+                              </div>
+                              <div class="form-group">
+                                <input type="email" class="form-control" name="email" placeholder="Email" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <input type="password" class="form-control" name="confirm-password" placeholder="Confirm Password" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+                              </div>
+                            </form>
+                            <div class="hint-text">Already have an account?<a href="#">Login here</a></div>
+                          </div>
 			</div>
 		)
 	}
