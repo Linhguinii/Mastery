14327f213ef83f91b5fbb3f6f6ca817d9e4e5d0e
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-08-02 19:41:16 -0400

    login.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-ro
 import 'bootstrap/dist/css/bootstrap.css';
 import Timer from "./Timer";
 import About from "./About";
+import LogIn from "./LogIn";
 
 class App extends React.Component{
   constructor(props){
@@ -74,53 +75,6 @@ class App extends React.Component{
  
 	)
   }
-}
-
-
-
-class LogIn extends React.Component{
-	constructor(props){
-		super(props);
-                this.body_css = {
-                  background: "#3598dc"
-                }
-                this.signup_form_css = {
-                  width: "400px",
-                  margin: "30px auto",
-                  marginTop: "0px",
-                  height: "100vh"
-                }
-	}
-	render(){
-		return(
-			<div style={this.body_css}>
-                          <div class="signup-form" style={this.signup_form_css}>
-                            <form action="" method="post">
-                              <h2>Create your account</h2>
-                              <div class="form-group">
-                                <div class="row">
-                                  <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></input></div>
-                                  <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></input></div>
-                                </div>
-                              </div>
-                              <div class="form-group">
-                                <input type="email" class="form-control" name="email" placeholder="Email" required="required"></input>
-                              </div>
-                              <div class="form-group">
-                                <input type="password" class="form-control" name="password" placeholder="Password" required="required"></input>
-                              </div>
-                              <div class="form-group">
-                                <input type="password" class="form-control" name="confirm-password" placeholder="Confirm Password" required="required"></input>
-                              </div>
-                              <div class="form-group">
-                                <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
-                              </div>
-                            </form>
-                            <div class="hint-text">Already have an account? <a href="#">Login here</a></div>
-                          </div>
-			</div>
-		)
-	}
 }
 	
 export default App;	
---
+++ src/LogIn.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class LogIn extends React.Component{
+	constructor(props){
+		super(props);
+                this.body_css = {
+                  background: "#3598dc"
+                }
+                this.signup_form_css = {
+                  width: "400px",
+                  margin: "30px auto",
+                  marginTop: "0px",
+                  height: "100vh"
+                }
+	}
+	render(){
+		return(
+			<div style={this.body_css}>
+                          <div class="signup-form" style={this.signup_form_css}>
+                            <form action="" method="post">
+                              <h2>Create your account</h2>
+                              <div class="form-group">
+                                <div class="row">
+                                  <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></input></div>
+                                  <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></input></div>
+                                </div>
+                              </div>
+                              <div class="form-group">
+                                <input type="email" class="form-control" name="email" placeholder="Email" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <input type="password" class="form-control" name="password" placeholder="Password" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <input type="password" class="form-control" name="confirm-password" placeholder="Confirm Password" required="required"></input>
+                              </div>
+                              <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-lg">Sign Up</button>
+                              </div>
+                            </form>
+                            <div class="hint-text">Already have an account? <a href="#">Login here</a></div>
+                          </div>
+			</div>
+		)
+	}
+}
+
+export default LogIn;
